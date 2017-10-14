cc5ff8421bcf041b485d421f552a90e7e4f3ebee
Edward Dupre <edwd42@gmail.com>
2017-10-14 14:54:35 -0400

    first commit

---
+++ ExercisesSection5Lecture42/ExercisesSection5Lecture42/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercisesSection5Lecture42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number > 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
