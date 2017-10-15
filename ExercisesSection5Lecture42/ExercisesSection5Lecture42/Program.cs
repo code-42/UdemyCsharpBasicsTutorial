ce9b3f154c2f94939e0fd80956ed8e3380f26afd
Edward Dupre <edwd42@gmail.com>
2017-10-14 20:47:01 -0400

    completed Exercises Section 5 Lecture 42

--- ExercisesSection5Lecture42/ExercisesSection5Lecture42/Program.cs
+++ ExercisesSection5Lecture42/ExercisesSection5Lecture42/Program.cs
@@ -8,9 +8,11 @@ namespace ExercisesSection5Lecture42
     {
         static void Main(string[] args)
         {
+
+             
             /*
              * 1- Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in applications where values entered into input boxes need to be validated.)
-s             */
+s            */
             Console.Write("Enter a number:");
             int number;
             number = Convert.ToInt32(Console.ReadLine());
@@ -34,6 +36,55 @@ s             */
             int max = a > b ? a : b;
             Console.WriteLine("Max number is " + max);
 
+            /**
+             * 3- Write a program and ask the user to enter the width and height of an image. Then tell if the image is landscape or portrait.
+             */
+            Console.Write("Enter the width and height of an image: ");
+            string dimensions = Console.ReadLine();
+            string[] valuez = dimensions.Split(' ');
+            int width = Convert.ToInt32(valuez[0]);
+            int height = Convert.ToInt32(valuez[1]);
+            //int orientation = width > height ? Console.WriteLine("Image is landscape") : Console.WriteLine("Image is portrait");
+            if (width > height)
+            {
+                Console.WriteLine("Image is landscape");
+            }
+            else
+            {
+                Console.WriteLine("Image is portrait");
+            }
+
+            /*
+             * 4- Your job is to write a program for a speed camera. For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic. Write a program that asks the user to enter the speed limit. Once set, the program asks for the speed of a car. If the user enters a value less than the speed limit, program should display Ok on the console. If the value is above the speed limit, the program should calculate the number of demerit points. For every 5km/hr above the speed limit, 1 demerit points should be incurred and displayed on the console. If the number of demerit points is above 12, the program should display License Suspended.
+             */
+            Console.Write("Enter the speed limit: ");
+            int speedLimit;
+            speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the speed of car: ");
+            int carSpeed;
+            carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                int over = carSpeed - speedLimit;
+                double demerits = over / 5;
+
+                if (demerits > 12)
+                {
+                    Console.WriteLine("License Suspended.");
+                }
+                else
+                {
+                    Console.WriteLine("Demerits: " + over/5);
+                }
+                
+                
+            }
         }
     }
 }
