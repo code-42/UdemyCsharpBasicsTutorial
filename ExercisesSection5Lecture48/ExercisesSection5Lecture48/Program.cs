5e91847af37b553099a67c5e385605e017b1eb57
Edward Dupre <edwd42@gmail.com>
2017-10-14 22:47:36 -0400

    add comments to program file

--- ExercisesSection5Lecture48/ExercisesSection5Lecture48/Program.cs
+++ ExercisesSection5Lecture48/ExercisesSection5Lecture48/Program.cs
@@ -7,6 +7,8 @@ namespace ExercisesSection5Lecture48
     {
         static void Main(string[] args)
         //public void Exercise1()
+        //   1- Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder.Display the count on the console.
+
         {
             int count = 0;
             for (var i = 1; i < 100; i++)
@@ -21,6 +23,7 @@ namespace ExercisesSection5Lecture48
             Exercise5();
         }
 
+        // 2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
         private static void Exercise2()
         {
             var sum = 0;
@@ -39,6 +42,7 @@ namespace ExercisesSection5Lecture48
 
         }
 
+        // 3- Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
         private static void factorial()
         {
             Console.Write("Enter a number: ");
@@ -51,6 +55,8 @@ namespace ExercisesSection5Lecture48
             Console.WriteLine("{0}! = {1}", number, factorial);
         }
 
+
+        // 4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
         private static void Exercise4()
         {
             var random = new Random().Next(1, 10);
@@ -68,6 +74,8 @@ namespace ExercisesSection5Lecture48
             Console.WriteLine("You lose");
         }
 
+
+        // 5- Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
         private static void Exercise5()
         {
             Console.WriteLine("Enter some numbers separated by commas: ");
