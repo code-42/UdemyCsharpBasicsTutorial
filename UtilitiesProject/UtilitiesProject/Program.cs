34377d8f1ef8f7c0911e73e8321a4c6d4e389d9b
Edward Dupre <edwd42@gmail.com>
2017-10-15 19:56:45 -0400

    created StringSummarizer utility

---
+++ UtilitiesProject/UtilitiesProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UtilitiesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is a really very very long sentence.";
+            var summary = StringSummarizer.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+    }
+}
