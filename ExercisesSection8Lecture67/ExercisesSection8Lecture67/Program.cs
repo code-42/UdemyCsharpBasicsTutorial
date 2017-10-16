9a8272ce6c65e648187e57b1587d7c76e7249ab4
Edward Dupre <edwd42@gmail.com>
2017-10-16 11:38:53 -0400

    completed Exercises Section 8 Lecture 67 Exercise 3

--- ExercisesSection8Lecture67/ExercisesSection8Lecture67/Program.cs
+++ ExercisesSection8Lecture67/ExercisesSection8Lecture67/Program.cs
@@ -12,7 +12,8 @@ namespace ExercisesSection8Lecture67
             byte asc = Convert.ToByte('-');
             Console.WriteLine(asc);
             //Exercise1();
-            Exercise2();
+            //Exercise2();
+            Exercise3();
         }
 
         // 1- Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
@@ -47,7 +48,6 @@ namespace ExercisesSection8Lecture67
 
 
         // 2- Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
-
         public static void Exercise2()
         {
             Console.WriteLine("Enter a few numbers separated by a hyphen (ex. 5-6-7-8): ");
@@ -66,6 +66,36 @@ namespace ExercisesSection8Lecture67
                 }
                 numbers.Add(Convert.ToInt32(number));
             }
+        }
+
+        // 3- Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
+        public static void Exercise3()
+        {
+            Console.WriteLine("enter a time value in the 24-hour time format (e.g. 19:00): ");
+            var input = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+            if (input.IndexOf(':') == -1)
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            var timeArr = input.Split(':');
+            var hour = Convert.ToByte(timeArr[0]);
+            var min = Convert.ToByte(timeArr[1]);
+            if (hour > 24  || (hour < 24 && min > 59))
+            {
+                Console.WriteLine("Invalid Time");
+                return;
+            }
+
+            //var time = new DateTime();
+            //time = DateTime.Parse(input);
+            Console.WriteLine("Ok");
         }
     }
 }
