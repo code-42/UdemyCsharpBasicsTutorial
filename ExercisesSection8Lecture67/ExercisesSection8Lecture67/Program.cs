29d678a7afb2bafa302e89df3d8c159b11645f73
Edward Dupre <edwd42@gmail.com>
2017-10-16 10:46:00 -0400

    completed Exercises Section 8 Lecture 67 Exercise 2

--- ExercisesSection8Lecture67/ExercisesSection8Lecture67/Program.cs
+++ ExercisesSection8Lecture67/ExercisesSection8Lecture67/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ExercisesSection8Lecture67
 {
@@ -10,7 +11,8 @@ namespace ExercisesSection8Lecture67
             Console.WriteLine("Hello World!");
             byte asc = Convert.ToByte('-');
             Console.WriteLine(asc);
-            Exercise1();
+            //Exercise1();
+            Exercise2();
         }
 
         // 1- Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
@@ -43,5 +45,27 @@ namespace ExercisesSection8Lecture67
             Console.WriteLine(message);
         }
 
+
+        // 2- Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
+
+        public static void Exercise2()
+        {
+            Console.WriteLine("Enter a few numbers separated by a hyphen (ex. 5-6-7-8): ");
+            var input = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(input))
+                return;
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split('-'))
+            {
+                var dupItems = numbers.Contains(Convert.ToInt32(number));
+                if (dupItems)
+                {
+                    Console.WriteLine("Duplicate found");
+                    break;
+                }
+                numbers.Add(Convert.ToInt32(number));
+            }
+        }
     }
 }
