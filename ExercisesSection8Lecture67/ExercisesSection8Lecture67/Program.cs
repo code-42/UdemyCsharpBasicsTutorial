8be9afbed7f82ebdeb77b3fac8354c54e80e262d
Edward Dupre <edwd42@gmail.com>
2017-10-16 21:22:26 -0400

    completed Procedural Programming Exercises Section 8 Lecture 68 Exercise 1

--- ExercisesSection8Lecture67/ExercisesSection8Lecture67/Program.cs
+++ ExercisesSection8Lecture67/ExercisesSection8Lecture67/Program.cs
@@ -9,14 +9,11 @@ namespace ExercisesSection8Lecture67
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            byte asc = Convert.ToByte('-');
-            Console.WriteLine(asc);
-            //Exercise1();
+            Exercise1();
             //Exercise2();
             //Exercise3();
             //Exercise4();
-            Exercise5();
+            //Exercise5();
         }
 
         // 1- Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".
@@ -25,6 +22,14 @@ namespace ExercisesSection8Lecture67
             Console.WriteLine("Enter a few numbers separated by a hyphen (ex. 5-6-7-8): ");
             var input = Console.ReadLine();
 
+            bool isConsecutive = IsConsecutiveList(input);
+
+            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
+        }
+
+        public static bool IsConsecutiveList(string input)
+        {
             var inStr = new List<int>();
             foreach (var ch in input.Split('-'))
             {
@@ -44,11 +49,8 @@ namespace ExercisesSection8Lecture67
                     break;
                 }
             }
-
-            var message = isConsecutive ? "Consecutive" : "Not Consecutive";
-            Console.WriteLine(message);
+            return isConsecutive;
         }
-
 
         // 2- Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.
         public static void Exercise2()
@@ -68,6 +70,7 @@ namespace ExercisesSection8Lecture67
                     break;
                 }
                 numbers.Add(Convert.ToInt32(number));
+                
             }
         }
 
